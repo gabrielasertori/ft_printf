533d8a993c992ac355b3f59fed1d261f661c0415
gabriela <gabriela@sertori.com.br>
2021-10-21 19:54:59 -0300

    test main.c

--- main.c
+++ main.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "ft_printf.h"
 
-void	print_numbers(int n, ...)
-{
-	va_list list;
 
-	va_start(list, n);
-
-	for (int i = 0; i < n; i++)
-	{
-		int	value = va_arg(list, int);
-		printf("%d: %d\n", i, value);
-	}
-
-	va_end(list);
-}
 int	main(void)
 {
-	print_numbers(2, 2, 4);
+	ft_printf("test");
+	printf("test");
 	return (0);
 }
